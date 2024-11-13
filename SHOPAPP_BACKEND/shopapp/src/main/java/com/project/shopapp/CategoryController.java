778ebf1537ac617a53b22bdcfe8e653351605c1f
PhongNguyen <Az@123321456>
2024-11-13 09:38:50 +0700

    init project

---
+++ SHOPAPP_BACKEND/shopapp/src/main/java/com/project/shopapp/CategoryController.java
@@ -0,0 +1,11 @@
+package com.project.shopapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/categorys")
+public class CategoryController {
+    //Hiện thi tất cả categoey
+
+}
